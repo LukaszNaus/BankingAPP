4d2690fb3d2161c3acb2bf469d2f26f9a6ea845e
LukaszNaus <nauslukasz@gmail.com>
2022-10-03 18:45:12 +0200

    create class account

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Account {
+   private String name;
+   private String surname;
+   private int accountNumber;
+   private float balance;
+
+
+ Account(String name, String surname){
+    this.name=name;
+    this.surname=surname;
+    this.accountNumber=1;
+    this.balance=100;
+
+}
+}
